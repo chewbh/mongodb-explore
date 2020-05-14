37833584c102cb49c438bbac75e1961140faef9a
marcus <marcuslgd@gmail.com>
2020-05-14 02:13:07 -0700

    ammend destroy script to remove pmm clients as well

--- install/ansible/destroy_cluster.sh
+++ install/ansible/destroy_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ## download and install os packages needed as part of ansible run (--tags "os_pkgs_only")
-ansible-playbook -i inventory -v 01-mongodb-cluster-setup.yml --vault-password-file=.vault.pwd --extra-vars '@sudo-secret' --extra-vars 'docker_container_remove=yes' --tags='all, wipe_data'
+ansible-playbook -i inventory -v 01-mongodb-cluster-setup.yml --vault-password-file=.vault.pwd --extra-vars '@sudo-secret' --extra-vars 'docker_container_remove=yes' --extra-vars "pmm_agent_setup=true|bool" --tags='all, wipe_data'
 
